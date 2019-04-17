97157144e7303dc8481859c196a431f020de759b
Gabe Villalobos <gvillalobos@scalecomputing.com>
2019-04-17 09:52:49 -0700

    Removed extra src directory

--- src/derpy/lib.rs
+++
@@ -1,2 +0,0 @@
-pub mod red_black_tree;
-mod red_black_tree_tests;
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-extern crate derpy_lib;
-use derpy_lib::red_black_tree::RedBlackTree;
-
-fn main() {
-    let mut rb_tree = RedBlackTree::new();
-    rb_tree.insert(42);
-}
