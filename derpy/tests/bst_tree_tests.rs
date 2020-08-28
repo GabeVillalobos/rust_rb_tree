ee1a7e03a4f8c15b3ba727c1a26250afbf1f95e5
Gabe Villalobos <gabriel_villalobos@outlook.com>
2020-08-28 10:28:46 -0700

    Adds a new inorder successor edge case test
    Specifically tests the scenario where a replacement inorder
      successor has a right child node, which itself contains
      a left child. This test is to verify that the subtree
      stays intact after removing the grandparent.

--- derpy/tests/bst_tree_tests.rs
+++ derpy/tests/bst_tree_tests.rs
@@ -2,10 +2,23 @@ extern crate derpy;
 use derpy::binary_search_tree::BinarySearchTree;
 
 fn verify_tree_bfs(bst: &mut BinarySearchTree<i32>, expected_vals: Vec<i32>) {
-    assert_eq!(bst.get_size(), expected_vals.len());
+    assert!(
+        bst.get_size() == expected_vals.len(),
+        "Expected tree size to be {}, but it was {}",
+        expected_vals.len(),
+        bst.get_size()
+    );
+
     let mut b_tree_iter = bst.bfs_iter();
     for val in expected_vals {
-        assert_eq!(b_tree_iter.next(), Some(&val));
+        let node = b_tree_iter.next();
+
+        assert!(
+            node == Some(&val),
+            "Expected node to be {:?}, but it was {:?}",
+            Some(&val),
+            node
+        );
     }
     assert_eq!(b_tree_iter.next(), None);
 }
@@ -141,6 +154,18 @@ fn remove_node() {
     println!("Remove node with 2 children, without an inorder successor");
     assert_eq!(b_tree.remove(&97).is_ok(), true);
     expected_order = vec![65, 44, 99, 66, 100];
+    verify_tree_bfs(&mut b_tree, expected_order);
+
+    println!("Remove node with inorder successor with right subtree");
+    b_tree.insert(110);
+    b_tree.insert(109);
+
+    // Verify that the tree is as we expect before testing
+    expected_order = vec![65, 44, 99, 66, 100, 110, 109];
+    verify_tree_bfs(&mut b_tree, expected_order);
+
+    assert_eq!(b_tree.remove(&99).is_ok(), true);
+    expected_order = vec![65, 44, 100, 66, 110, 109];
     verify_tree_bfs(&mut b_tree, expected_order);
 }
 
