d116c4befa41a9eadd98815af7ca8e3f4269f37c
Gabe Villalobos <gabriel_villalobos@outlook.com>
2020-09-03 01:42:57 -0700

    Adds rotations + recolors for remaining RB Tree insert cases
    Mind you, none of this is tested so it may not actually work ¯\_(ツ)_/¯

--- derpy/src/red_black_tree.rs
+++ derpy/src/red_black_tree.rs
@@ -97,22 +97,95 @@ impl<T: PartialOrd + Display + Default> RedBlackTree<T> {
                 if grandparent_idx_opt.is_none() {
                     return;
                 }
-                let grandparent_idx = grandparent_idx_opt.unwrap();
+
                 let node_is_left_child =
                     self.bst.nodes[parent_idx_opt.unwrap()].left == node_idx_opt;
                 // 4 possible cases here:
                 match (parent_is_left_child, node_is_left_child) {
                     // 1: parent is left child, node is left child
-                    (true, true) => {},
+                    (true, true) => {
+                        self.rotate_node_right(parent_idx_opt.unwrap());
+                    }
                     // 2: parent is left child, node is right child
-                    (true, false) => {},
+                    (true, false) => {
+                        self.rotate_node_left(node_idx_opt.unwrap());
+                        self.rotate_node_right(parent_idx_opt.unwrap());
+                    }
                     // 3: mirror of 1
-                    (false, true) => {},
+                    (false, true) => {
+                        self.rotate_node_left(parent_idx_opt.unwrap());
+                    }
                     // 5: mirror of 2
-                    (false, false) => {},
+                    (false, false) => {
+                        self.rotate_node_right(node_idx_opt.unwrap());
+                        self.rotate_node_left(parent_idx_opt.unwrap());
+                    }
                 }
+
+                // Swap grandparent & parent colors, and we're done!
+                let grandparent_color = self.get_node_color(grandparent_idx_opt);
+                let parent_color = self.get_node_color(parent_idx_opt);
+
+                self.set_node_color(parent_idx_opt, grandparent_color);
+                self.set_node_color(grandparent_idx_opt, parent_color);
             }
         };
+    }
+
+    // When rotating a node, we set the parent's right child equal to the
+    //  target node's left child. Then we set the parent as the left child
+    //  of the target. Finally we fix all of the parent references, et voila.
+    fn rotate_node_right(&mut self, node_idx: Index) {
+        let node = &mut self.bst.nodes[node_idx];
+        let parent_idx = node
+            .parent
+            .expect("Proper tree structure ensures that a occurs only on nodes with parents");
+        let left_node_idx_opt = node.left;
+        node.left = Some(parent_idx);
+
+        let parent_node = &mut self.bst.nodes[parent_idx];
+        parent_node.parent = Some(node_idx);
+        parent_node.right = left_node_idx_opt;
+
+        let grandparent_idx_opt = parent_node.parent;
+        self.bst.nodes[node_idx].parent = grandparent_idx_opt;
+
+        // If grandparent exists, we replace its child idx with the parent with the target node
+        if let Some(grandparent_idx) = grandparent_idx_opt {
+            let grandparent_node = &mut self.bst.nodes[grandparent_idx];
+            if grandparent_node.left == Some(parent_idx) {
+                grandparent_node.left = Some(node_idx);
+            } else {
+                grandparent_node.right = Some(node_idx);
+            }
+        }
+    }
+
+    fn rotate_node_left(&mut self, node_idx: Index) {
+        let node = &mut self.bst.nodes[node_idx];
+        let parent_idx = node
+            .parent
+            .expect("Proper tree structure ensures that a occurs only on nodes with parents");
+
+        let right_node_idx_opt = node.right;
+        node.right = Some(parent_idx);
+
+        let parent_node = &mut self.bst.nodes[parent_idx];
+        parent_node.parent = Some(node_idx);
+        parent_node.left = right_node_idx_opt;
+
+        let grandparent_idx_opt = parent_node.parent;
+        self.bst.nodes[node_idx].parent = grandparent_idx_opt;
+
+        // If grandparent exists, we replace its child idx with the parent with the target node
+        if let Some(grandparent_idx) = grandparent_idx_opt {
+            let grandparent_node = &mut self.bst.nodes[grandparent_idx];
+            if grandparent_node.left == Some(parent_idx) {
+                grandparent_node.left = Some(node_idx);
+            } else {
+                grandparent_node.right = Some(node_idx);
+            }
+        }
     }
 
     fn get_node_color(&self, node_opt: Option<Index>) -> TreeColors {
